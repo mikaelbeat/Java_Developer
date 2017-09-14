c1e3f2392894d06d35fe5e7adc023ac8123cbb47
mikaelbeat <ryynanenphm@gmail.com>
2017-09-14 10:32:03 +0300

    Database training

--- Udemy_Java_Developer/src/database/ConnectToDB.java
+++ Udemy_Java_Developer/src/database/ConnectToDB.java
@@ -13,8 +13,14 @@ public class ConnectToDB {
 					System.out.println("Connected successfully.\n");
 					
 					// Add users
-					System.out.println("Press 1 to add new user.");
-					System.out.println("Press 2 to show all users in database.");
+					System.out.println("Select function"
+										+ "\n--------------------"
+										+ "\n1 for adding user."
+										+ "\n2 for viewing all users."
+										+ "\n3 for deleting user."
+										+ "\n4 for updating password");
+					
+					Statement stmt=c.createStatement();
 					Scanner reader = new Scanner(System.in);
 					int processIndex = reader.nextInt();
 					switch (processIndex) {
@@ -26,7 +32,7 @@ public class ConnectToDB {
 						System.out.println("Enter password: ");
 						String password = PasswordInput.nextLine();
 						String SQLAdd="insert into admins(user_name,password) values('"+user_name+"','"+password+"')";
-						Statement stmt=c.createStatement();
+						
 						stmt.executeUpdate(SQLAdd);
 						c.commit(); // Add // Delete // Update
 						System.out.println("New user is added.");
@@ -35,9 +41,8 @@ public class ConnectToDB {
 						break;
 					case  2:
 						// Show all users from database
-						Statement stmt2=c.createStatement();
 						String SqlRead = "select * from admins";
-						ResultSet rs = stmt2.executeQuery(SqlRead);
+						ResultSet rs = stmt.executeQuery(SqlRead);
 						System.out.println("ID\tUSERNAME\tPASSWORD");
 						while(rs.next()){
 							int id = rs.getInt("id");
@@ -46,8 +51,38 @@ public class ConnectToDB {
 							System.out.println(id +"\t"+ UserNameRead+ "\t" + PasswordRead);
 						}
 						rs.close();
-						stmt2.close();
+						stmt.close();
 						c.close();
+						break;
+					case 3:
+						// Delete user
+						Scanner IdInput = new Scanner(System.in);
+						System.out.println("Enter user ID: ");
+						int id = IdInput.nextInt();
+						String SQLDelete = "delete from admins where id="+id;
+						
+						stmt.executeUpdate(SQLDelete);
+						stmt.close();
+						c.commit();
+						c.close();
+						System.out.println("User is deleted.");
+						break;
+					case 4:
+						// Update password
+						Scanner IdUpdate = new Scanner(System.in);
+						Scanner PasswordUpdate = new Scanner(System.in);
+						System.out.println("Enter ID: ");
+						int IdUp = IdUpdate.nextInt();
+						System.out.println("Enter new password: ");
+						String PasswordUp = PasswordUpdate.nextLine();
+						String SQLupdate = "UPDATE admins set password='"+PasswordUp+"'"
+						+"where id="+IdUp;
+						
+						stmt.executeUpdate(SQLupdate);
+						stmt.close();
+						c.commit();
+						c.close();
+						System.out.println("User password is updated.");
 						break;
 					}
 					}
