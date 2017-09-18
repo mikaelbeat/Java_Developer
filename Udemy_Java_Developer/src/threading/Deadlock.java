a85e4d9cc00e7dee908c0660737a1332ab918314
mikaelbeat <ryynanenphm@gmail.com>
2017-09-18 13:50:15 +0300

    New class

--- Udemy_Java_Developer/src/threading/Deadlock.java
+++ Udemy_Java_Developer/src/threading/Deadlock.java
@@ -6,7 +6,39 @@ public class Deadlock {
 		
 		final String Passenger1="Passenger1";
 		final String Passenger2="Passenger2";
-
+		Thread bus1=new Thread(){
+			@Override
+			public void run(){
+				synchronized (Passenger1){
+					System.out.println("Bus 1 is locked passenger 1.");
+					try{
+					Thread.sleep(1000);
+					}catch(Exception ex){}
+					synchronized (Passenger2) {
+						System.out.println("Bus 1 is locked passenger 2.");
+					}
+				}
+			}
+		};
+				
+		Thread bus2=new Thread(){
+			@Override
+			public void run(){
+				synchronized (Passenger2){
+					System.out.println("Bus 1 is locked passenger 2.");
+					try{
+					Thread.sleep(1000);
+					}catch(Exception ex){}
+					synchronized (Passenger1) {
+						System.out.println("Bus 1 is locked passenger 1.");
+					}
+				}
+			}
+		};
+		
+		bus1.start();
+		bus2.start();
+		
 	}
 
 }
